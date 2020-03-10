32b4fbf7255d1ba5d27f6703bc40604a4eb0e575
hangi12345 <z5162101@ad.unsw.edu.au>
2020-03-10 16:16:22 +1100

    nearly completed bs4 for scraping data

--- PHASE_1/API_SourceCode/web_scrapper.py
+++ PHASE_1/API_SourceCode/web_scrapper.py
@@ -10,20 +10,36 @@ src = result.content
 
 soup = BeautifulSoup(src, 'html.parser')
 links = []
-prefix = "https://www.who.int/"
-end = 0
-
+overal = []
+prefix = "https://www.who.int"
+# Goes through all the years
 for link in soup.findAll('a', attrs={'href': re.compile("/csr/don/archive/year/[1-2][0-9]{3}/.*")}):
     links.append(prefix + link.get('href'))
-    end = end + 1
-print(links)
 
-counter = 0
-for counter in  range(end):
-    newResult = requests.get(links[0])
-    print(newResult)
-#for year in links:
- #   print(year)
-  #  print("\n")
+# Enter the href link to each year
+for coverage in links:
+    newResult = requests.get(coverage)
+    src2 = newResult.content
+    newSoup = BeautifulSoup(src2, 'html.parser')
+    # get the href link for each report for the year and store all of it in list overal
+    for reports in newSoup.findAll('a', attrs={'href': re.compile("/csr/don/[0-9]{2}.*")}):
+        overal.append(prefix + reports.get('href'))
+# Removes any duplication of url in this run
+overal = list(dict.fromkeys(overal))
+
+# Enter the href link to the report to extract the data required
+for page in overal:
+    lastResult = requests.get(page)
+    src3 = lastResult.content
+    lastSoup = BeautifulSoup(src3, 'html.parser')
+    # Within the report get the title,date,content,disease,syndrome and location
+    report = {
+        'url':
+        'title':
+        'date':
+        'content':
+    }
+
+
 
 

76575e3685873ab848e228b45f432a7989f4776c
niki <niki.bolourchi@gmail.com>
2020-04-02 00:19:48 +1100

    some progress on adding the list of diseases

--- PHASE_2/Application_SourceCode/frontend/src/Pages/Diseases.js
+++ PHASE_2/Application_SourceCode/frontend/src/Pages/Diseases.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../App.css';
+import data from '../Data/disease_list';
 
-function Diseases() {
-    return (
-        <div>
-            <center className="Title"> All Diseases </center>
-        </div>
-    );
+class Diseases extends Component {
+	render(){
+	    return (
+	        <div>
+	            <center className="Title"> All Diseases </center>
+	            {data.map((item, index) => {
+	            return <div>
+	            	<h1>(item.name)</h1>
+	            </div>
+	            })}
+
+	        </div>
+	    );
+	}
 }
 
 export default Diseases;

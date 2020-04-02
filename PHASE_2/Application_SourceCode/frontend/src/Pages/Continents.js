0b236499ae3cddde8912f2afd4e199959a8a7e2c
niki <niki.bolourchi@gmail.com>
2020-04-03 00:27:53 +1100

    load continents

--- PHASE_2/Application_SourceCode/frontend/src/Pages/Continents.js
+++ PHASE_2/Application_SourceCode/frontend/src/Pages/Continents.js
@@ -2,12 +2,22 @@ import React, {Component} from 'react';
 import '../App.css';
 import data from '../Data/continents.json';
 
-function Continents() {
-    return (
-        <div>
-            <center className="Title"> All Continents </center>
-        </div>
-    );
+class Continents extends Component {
+    render(){
+        let item = data.map(continent=>
+                <div id={continent.countries}>
+                {continent.countries}
+                </div>
+            );
+        return (
+            <div>
+                <center className="Title"> All Continents</center>
+                <div id="disease-names">
+                {item}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Continents;

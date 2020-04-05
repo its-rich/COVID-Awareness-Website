7f52bc363398623a0f8a3cc7aefa295fbb0ee420
Richard Liu <z5165455@student.unsw.edu.au>
2020-04-06 02:34:28 +1000

    fixed bugs

--- PHASE_2/Application_SourceCode/frontend/src/App.js
+++ PHASE_2/Application_SourceCode/frontend/src/App.js
@@ -7,12 +7,9 @@ import Diseases from './Pages/Diseases.js';
 import Home from './Pages/Home.js';
 import Reports from './Pages/Reports.js';
 import News from './Pages/News.js';
-<<<<<<< HEAD
 import DatePicker from './components/DatePicker.js';
 import PickDate from './components/PickDate.js';
-=======
 import Risk from './Pages/Risk.js';
->>>>>>> 8a27f7c4764fcc1b1a2165f3329fa1b08b585e83
 
 function App () {
     return (
@@ -20,21 +17,18 @@ function App () {
             <div className="App">
                 <Nav />
                 <Switch>
-                    <PickDate />
-                    {/* <Route path="/" exact component={Home} />
+                    <Route path="/" exact component={Home} />
                     <Route path="/diseases" exact component={Diseases} />
                     <Route path="/continents" exact component={Continents} />
                     <Route path="/reports" exact component={Reports} />
                     <Route path="/latestnews" exact component={News} />
-<<<<<<< HEAD
-                    <Route path="/dates" exact component={DatePicker} /> */}
-=======
+                    <Route path="/dates" exact component={DatePicker} />
                     <Route path="/risk" exact component={Risk} />
->>>>>>> 8a27f7c4764fcc1b1a2165f3329fa1b08b585e83
                 </Switch>
             </div>
         </Router>
     );
 }
+    // <PickDate />
 
 export default App

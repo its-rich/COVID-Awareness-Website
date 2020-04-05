d830c21046c9045ccdadbe44a24b68df3c991af5
niki <niki.bolourchi@gmail.com>
2020-04-06 00:59:52 +1000

    disease page style change

--- PHASE_2/Application_SourceCode/frontend/src/Pages/AllDiseases.js
+++ PHASE_2/Application_SourceCode/frontend/src/Pages/AllDiseases.js
@@ -11,7 +11,7 @@ class AllDiseases extends Component {
     render() {
         let item = data.map(disease =>
                 <div className="alldiseases" key={disease.name} onClick={this.changeDisease.bind(this)}>
-                <button>{disease.name}</button>
+               <h4>{disease.name}</h4>
                 </div>
             );
         return (

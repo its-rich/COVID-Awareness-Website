ff61110ed8397de9f36138d3a7957468be0dd41d
niki <niki.bolourchi@gmail.com>
2020-04-05 22:00:30 +1000

    select continent button

--- PHASE_2/Application_SourceCode/frontend/src/Pages/Continents.js
+++ PHASE_2/Application_SourceCode/frontend/src/Pages/Continents.js
@@ -42,7 +42,7 @@ class Continents extends Component {
 
             <div className="Continentwrapper">
                 <center className="Title"> <h3>Country Page</h3> </center>
-               <div className="continentfield"> <button align="centre" onClick={this.showDropdown}>  Continent</button> </div>
+               <div className="continentfield"> <button align="centre" onClick={this.showDropdown}>  Select Continent</button> </div>
                 {
                     this.state.showDropdown
                         ? (

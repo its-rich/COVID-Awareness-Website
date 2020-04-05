2cede53a3022d1e46cf622f195186bc7aa6b9fe0
Patrick <z5161379@student.unsw.edu.au>
2020-04-06 01:27:42 +1000

    super jank but better workig risk graph

--- PHASE_2/Application_SourceCode/frontend/src/components/RiskGraph.js
+++ PHASE_2/Application_SourceCode/frontend/src/components/RiskGraph.js
@@ -73,7 +73,47 @@ class RiskGraph extends React.Component {
 
     }
 
+    UNSAFE_componentWillReceiveProps(){
+            let newData = [['Date','Cases']];
+            var date = new Date(1577836800000);
+
+            let i = 0;
+
+            cases.forEach((newCase) => {
+                if (newCase.postcode == this.props.postcode){
+                    let newDate = new Date(newCase.notification_date);
+                    //console.log(date);
+                    //console.log(newDate);
+                    //console.log(date > newDate);
+                    while (date < newDate){
+                        let dateString = date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate();
+                        let temp = [dateString,i];
+                        newData.push(temp);
+                        date = new Date( date.getTime() + 86400000);
+                        //console.log(temp);
+                    }
+                    i = i + 1;
+                    
+                    let temp;
+                    temp = [newCase.notification_date,i];
+                    newData.push(temp);
+                }
+           
+            });
+            var today = new Date();
+            while (date < today){
+                let dateString = date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate();
+                let temp = [dateString,i];
+                date = new Date( date.getTime() + 86400000);
+            }
+            //console.log(newData);
+            this.setState({data: newData});
+//            console.log("Unable to connect to NSW gov, using stored statistics");
+//        }
+    }
+
     render() {
+        console.log(this.state.data)
         return (
             <Chart
               width={'1000px'}
@@ -82,7 +122,7 @@ class RiskGraph extends React.Component {
               loader={<div>Loading Chart</div>}
               data={this.state.data}
               options={{
-                title: "Postcodes with most infections of COVID-19 in NSW"
+                title: "Rate of Infection in Postcode: " + this.props.postcode
               }}
               rootProps={{ 'data-testid': '1' }}
             />

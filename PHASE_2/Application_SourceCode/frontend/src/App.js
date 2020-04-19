22c0fecc5ba6516de6d7bb418ba8ebd528766061
niki <niki.bolourchi@gmail.com>
2020-04-19 15:05:36 +1000

    private routing for sign in

--- PHASE_2/Application_SourceCode/frontend/src/App.js
+++ PHASE_2/Application_SourceCode/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Nav from './components/Navigation/Nav.js';
 import './App.css';
 import VirusAvoider from './Pages/VirusAvoider.js';
@@ -8,8 +8,26 @@ import Infected from './Pages/Infected.js'
 import Home from './Pages/Home.js';
 import Register from './Pages/Register.js';
 import Login from './Pages/SignIn.js';
+import { db } from './components/Firebase/config.js'
+
 
 function App () {
+    const PrivateRoute = ({ children, ...rest }) => {
+  return (
+        <Route {...rest}
+                render={({ location })=>
+                db.currentConnection != null ? (
+                  children
+                ) : (
+            <Redirect to={{
+              pathname: "/login",
+              state: { from: location }
+            }} />
+        )
+      }
+    />
+  );
+};
     return (
         <Router>
             <div className="App">
@@ -19,6 +37,9 @@ function App () {
                     <Route path="/coronavirus-avoider" exact component={VirusAvoider} />
                     <Route path="/coronavirus-simulator" exact component={VirusSimulator} />
                     <Route path="/infected" exact component={Infected} />
+                    <PrivateRoute exact path="/">
+                     <Infected />
+                    </PrivateRoute>
                     <Route path="/register" exact component ={Register} />
                     <Route path="/SignIn" exact component ={Login} />
                 </Switch>
